0dd7e49bfd4feb995ef8e70f4a9b5281c3003f43
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-16 23:42:11 +0900

    Add styles

--- resources/assets/js/components/TodoList/TodoListItem.vue
+++ resources/assets/js/components/TodoList/TodoListItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <section>{{ todo }}</section>
+  <li class="todo-item">
+    <input class="todo-item__is-done" type="checkbox" :checked="todo.isDone" />
+    <input class="todo-item__task" type="text" :value="todo.task" :disabled="todo.isDone" />
+    <button class="todo-item__remove">[x]</button>
+  </li>
 </template>
 
 <script>
@@ -11,4 +15,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../../sass/variables";
+
+.todo-item {
+  list-style: none;
+  padding: 0;
+
+  &__is-done {
+    background: none;
+  }
+
+  &__task,
+  &__remove {
+    border: 0;
+    outline: 0;
+    background: none;
+    transition: 0.1s;
+  }
+
+  &__task {
+    padding-left: 1rem;
+    border: 1px solid rgba($color: $text-color, $alpha: 0);
+    border-radius: 10px;
+
+    &:hover {
+      border-color: rgba($color: $text-color, $alpha: 0.3);
+    }
+
+    &:focus {
+      border-color: rgba($color: $text-color, $alpha: 1);
+    }
+
+    &:disabled {
+      border: none;
+      text-decoration: line-through;
+    }
+  }
+}
 </style>
